233772aabe34770f755df3834a5b3aa24ad93e2e
Thalles Bruno <thallesbgns@gmail.com>
2020-10-06 22:22:01 -0300

    organizacao JS

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,13 @@
+console.log("Sentença de código");
+
+{
+    {
+        {
+            console.log("Olá");
+            console.log('Mundo!') //padrão do curso
+        }
+    }
+}
+
+//sentenças de código, que podem ou não ser finalizadas com ;
+//blocos de código
