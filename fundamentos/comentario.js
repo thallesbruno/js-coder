9ccc59c75ad0e834e00a232aa8c8bcf1906d3280
Thalles Bruno <thallesbgns@gmail.com>
2020-10-07 12:09:30 -0300

    comentarios. var, let e const

---
+++ fundamentos/comentario.js
@@ -0,0 +1,22 @@
+//Comentário de uma linha
+console.log('Linha 1');
+
+/*
+Comentário de
+várias linhas
+*/
+console.log('Linha 2');
+
+/*
+*Comentário
+*de
+*múltiplas linhas
+*
+*
+*
+*/
+console.log('Linha 3');
+
+//Evite comentários idiotas/redundantes (que repetem algo que já é claro)
+//Preze pelo código, um código claro e de fácil leitura
+//Comente o que sentir necessidade de comentar (contexto educacional)
