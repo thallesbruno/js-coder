fbf2e116f414563b49078c5b9b235aa39c938c2e
Thalles Bruno <thallesbgns@gmail.com>
2020-10-14 12:02:45 -0300

    operadores ternários

---
+++ fundamentos/ternario.js
@@ -0,0 +1,8 @@
+//operador ternário - com 3 operandos
+
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+//expressão relacional + resultado se verdadeiro + resultado se falso
+
+console.log(resultado(7.1))
+console.log(resultado(6.9))
