50e25aba626f25f307a161fd4ac01ce7eab0a251
Thalles Bruno <thallesbgns@gmail.com>
2020-10-13 23:58:34 -0300

    aritmeticos

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,19 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d //sintaxe infix
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+
+//postfix
+//prefix
+//infix
+
+/*  console.log(a)
+    *console.log(b)
+    *console.log(c)
+    *console.log(d)
+*/
