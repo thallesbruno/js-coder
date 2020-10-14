eae3ed6f2cc49a0a62c20596150140f2f2fecbd7
Thalles Bruno <thallesbgns@gmail.com>
2020-10-13 23:33:52 -0300

    desestruturação em array e object

--- fundamentos/destructuring3.js
+++ fundamentos/destructuring3.js
@@ -6,5 +6,5 @@ function rand({min = 0, max = 1000}){
 const obj = { max: 50, min: 40 }
 console.log(rand({ max: 50, min: 40 }))
 console.log(rand({ min: 955 }))
-//console.log(rand({}))
+console.log(rand({}))
 //console.log(rand())
