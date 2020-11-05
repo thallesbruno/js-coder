d561fed82715ab58d73bb0dcfb575ea709fb842c
Thalles Bruno <thallesbgns@gmail.com>
2020-11-04 22:35:43 -0300

    funcoes 1

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,13 @@
+//first class object/citizen
+//funções como dados
+
+//criar de forma literal
+function fun1() { }
+
+//armazenar em uma variável
+const fun2 = function () { }
+
+//armazenar em um array
+const array = [function (a, b) { return a + b }, fun1, fun2]
+console.log(array[0](2, 3))
+
